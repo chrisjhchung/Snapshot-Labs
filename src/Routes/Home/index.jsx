e2aef72807f5377ada46881d0d866cdec90ce0af
Chris Chung <chrisjhchung@gmail.com>
2024-02-18 11:46:35 +1100

    feat: layout

--- src/Routes/Home/index.jsx
+++ src/Routes/Home/index.jsx
@@ -33,27 +33,67 @@ const Home = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "column",
       }}
     >
       <div
         style={{
-          maxWidth: "500px",
+          maxWidth: "1000px",
           lineHeight: "1.5",
           display: "flex",
           flexDirection: "column",
         }}
       >
         <h1>📸 Snapshot Labs</h1>
-        <p>
-          Snapshot Labs is a sandbox for testing different webpage screenshot
-          capture methods, including Server-Side Rendering (SSR) and CSS-only
-          calculations. It ensures accurate representation of CSS styles, custom
-          fonts, and dynamic page elements. The platform provides a simple API
-          for integration and is designed for scalability, capable of generating
-          over 100 images simultaneously, catering to developers needing
-          effective screenshot functionality in their web projects.
-        </p>
-        <i>The SPA that should be outputted is displayed on the right</i>
+        <div
+          style={{
+            lineHeight: "1.5",
+            display: "flex",
+            flexDirection: "row",
+            gap: "32px",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              maxWidth: "500px",
+              lineHeight: "1.5",
+              display: "flex",
+              flexDirection: "column",
+              gap: "24px",
+            }}
+          >
+            <p
+              style={{
+                margin: 0,
+              }}
+            >
+              Snapshot Labs is a sandbox for testing different webpage
+              screenshot capture methods, including Server-Side Rendering (SSR)
+              and CSS-only calculations. It ensures accurate representation of
+              CSS styles, custom fonts, and dynamic page elements. The platform
+              provides a simple API for integration and is designed for
+              scalability, capable of generating over 100 images simultaneously,
+              catering to developers needing effective screenshot functionality
+              in their web projects.
+            </p>
+            <i>The SPA that should be outputted is displayed on the right</i>
+          </div>
+
+          <div
+            style={{
+              height: "300px",
+              width: "300px",
+              background: "rgb(24, 24, 27)",
+              borderRadius: "8px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Example goes here
+          </div>
+        </div>
         <div className={"optionForm"}>
           <label style={{ display: "flex", flexDirection: "column" }}>
             <span> Images to generate (n= 1..100)</span>
