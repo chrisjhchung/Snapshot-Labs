0cbcf83906d3250b389205298ed39ba7cdfd1eb7
Chris Chung <chrisjhchung@gmail.com>
2024-02-18 12:04:55 +1100

    chore: more styles

--- src/Routes/Home/index.jsx
+++ src/Routes/Home/index.jsx
@@ -29,7 +29,7 @@ const Home = () => {
         height: "100svh",
         width: "100vw",
         background: "rgba(10,10,11)",
-        color: "rgba(248,248,248)",
+        color: "rgb(161, 161, 170)",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
@@ -45,22 +45,87 @@ const Home = () => {
         }}
       >
         <h1>📸 Snapshot Labs</h1>
+        <div className={"containerTop"}>
+          <div className={"optionForm"}>
+            <label style={{ display: "flex", flexDirection: "column" }}>
+              <span> Images to generate (n= 1..100)</span>
+              <input
+                type="number"
+                min="1"
+                max="100"
+                placeholder="100"
+                value={imageCount}
+                onChange={(e) => setImageCount(e.target.value)}
+              />
+            </label>
+            <label style={{ display: "flex", flexDirection: "column" }}>
+              <span> Choose strategy</span>
+              <select onChange={(e) => setStrategy(e.target.value)}>
+                <option value="css-injection">CSS Injection</option>
+                <option disabled value="satori">
+                  Satori (Not yet implemented)
+                </option>
+                <option disabled value="client-spa">
+                  SPA on client (Not yet implemented)
+                </option>
+                <option disabled value="pure">
+                  Pure HTML, JS, and CSS (Not yet implemented)
+                </option>
+              </select>
+            </label>
+            <button
+              onClick={() => {
+                console.log("hello");
+                generateImages();
+              }}
+            >
+              Generate Image
+            </button>
+          </div>
+
+          <div className="results">
+            <div
+              style={{
+                padding: "16px 24px",
+                display: "flex",
+                flexDirection: "column",
+                lineHeight: "1.75",
+              }}
+            >
+              <p style={{ margin: 0, marginBottom: "1rem", color: "white" }}>
+                Results
+              </p>
+              <p
+                style={{ margin: 0, fontSize: "14px" }}
+              >{`Images Generated: ${"N/A"}`}</p>
+              <p
+                style={{ margin: 0, fontSize: "14px" }}
+              >{`Time taken (ms): ${"N/A"}`}</p>
+              <p
+                style={{ margin: 0, fontSize: "14px" }}
+              >{`Memory Usage (%): ${"N/A"}`}</p>
+            </div>
+          </div>
+        </div>
+
         <div
           style={{
             lineHeight: "1.5",
             display: "flex",
             flexDirection: "row",
             gap: "32px",
-            alignItems: "center",
+            alignItems: "flex-start",
           }}
         >
           <div
             style={{
               maxWidth: "500px",
               lineHeight: "1.5",
+              height: "100%",
               display: "flex",
               flexDirection: "column",
               gap: "24px",
+              textAlign: "justify",
             }}
           >
             <p
@@ -82,8 +147,9 @@ const Home = () => {
 
           <div
             style={{
-              height: "300px",
-              width: "300px",
+              height: "400px",
+              flex: 1,
+              aspectRatio: "1 / 1",
               background: "rgb(24, 24, 27)",
               borderRadius: "8px",
               display: "flex",
@@ -93,42 +159,6 @@ const Home = () => {
           >
             Example goes here
           </div>
-        </div>
-        <div className={"optionForm"}>
-          <label style={{ display: "flex", flexDirection: "column" }}>
-            <span> Images to generate (n= 1..100)</span>
-            <input
-              type="number"
-              min="1"
-              max="100"
-              placeholder="100"
-              value={imageCount}
-              onChange={(e) => setImageCount(e.target.value)}
-            />
-          </label>
-          <label style={{ display: "flex", flexDirection: "column" }}>
-            <span> Choose strategy</span>
-            <select onChange={(e) => setStrategy(e.target.value)}>
-              <option value="css-injection">CSS Injection</option>
-              <option disabled value="satori">
-                Satori (Not yet implemented)
-              </option>
-              <option disabled value="client-spa">
-                SPA on client (Not yet implemented)
-              </option>
-              <option disabled value="pure">
-                Pure HTML, JS, and CSS (Not yet implemented)
-              </option>
-            </select>
-          </label>
-          <button
-            onClick={() => {
-              console.log("hello");
-              generateImages();
-            }}
-          >
-            Generate Image
-          </button>
         </div>
       </div>
     </div>
